67689343b92b655dbf7057aed3303637a6391245
Tushar Reddy <reddytushar295@outlook.com>
2024-10-10 14:00:16 +0530

    Added Upload result html

--- src/app.py
+++ src/app.py
@@ -5,8 +5,8 @@ import os
 app = Flask(__name__)
 
 S3_BUCKET = "imageflow899"
-S3_ACCESS_KEY = "	AKIAZQ3DNMSI3QYOK3M2"
-S3_SECRET_KEY = "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"
+S3_ACCESS_KEY = ""
+S3_SECRET_KEY = ""
 S3_REGION = 'us-east-1'
 
 s3 = boto3.client('s3', aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY)
@@ -26,16 +26,15 @@ def upload():
         s3.upload_fileobj(
             image,
             S3_BUCKET,
-            s3_file_name,
+            f"uploads/{s3_file_name}",
             ExtraArgs={
-                "ACL": 'public-read',
                 "ContentType": image.content_type
             }
         )
-        return f"Upload Successful: {s3_file_name}!"
+        return render_template('upload_result.html', success=True)
     except Exception as e:
-        print(f"Error: {e}")
-        return "Upload Failed"
+        #print(f"Error: {e}")
+        return render_template('upload_result.html', success=False , error=str(e))
     
 if __name__ == '__main__':
     app.run(debug=True)
